3b539feda8032f80cdab20036d0ebfda18bad24e
Yoni Nachmany <yoni.nachmany@radiant.earth>
2019-04-04 10:50:15 -0400

    cap

--- modeling/main/crossvalidation/experiment.py
+++ modeling/main/crossvalidation/experiment.py
@@ -113,7 +113,7 @@ early_stopping=keras.callbacks.EarlyStopping(monitor='val_loss',
 
 num_splits=10
 
-performancens={name:pd.DataFrame(columns=['empathy', 'distress'], 
+performancens={name:pd.DataFrame(columns=['Empathy', 'Distress'],
 	index=range(1,num_splits+1)) for name in MODELS.keys()}
 
 
